41f018156be081173a959fadd908508d6d0d13bd
Piotr Kniaź <pio.kniaz@gmail.com>
2022-01-01 16:37:23 +0100

    chore: paths alias jest and tsconfig update

--- packages/web/jest.config.js
+++ packages/web/jest.config.js
@@ -1,8 +1,14 @@
 module.exports = {
-  setupFilesAfterEnv: ['<rootDir>/src/test-utils/setupTests.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/utils/test-utils/setupTests.ts'],
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '^@pages(.*)$': '<rootDir>/src/pages/$1',
+    '^@assets(.*)$': '<rootDir>/src/assets/$1',
+    '^@components(.*)$': '<rootDir>/src/components/$1',
+    '^@helpers(.*)$': '<rootDir>/src/helpers/$1',
+    '^@redux(.*)$': '<rootDir>/src/redux/$1',
+    '^@routes(.*)$': '<rootDir>/src/routes/$1',
+    '^@styles(.*)$': '<rootDir>/src/styles/$1',
   },
 };
