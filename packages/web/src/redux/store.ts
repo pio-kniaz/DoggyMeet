0a5550d5f094940cdd2b374ab8f78fd1b257925b
Piotr Kniaź <piotrkniaz@Piotrs-MacBook-Pro.local>
2022-02-19 15:55:18 +0100

    feat: redux store update

--- packages/web/src/redux/store.ts
+++ packages/web/src/redux/store.ts
@@ -2,10 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import modalReducer from '@/redux/modal/modal.slice';
 
+export const rootReducer = {
+  modal: modalReducer,
+};
+
 export const store = configureStore({
-  reducer: {
-    modal: modalReducer,
-  },
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV !== 'production',
   middleware: [logger] as const,
 });
 
