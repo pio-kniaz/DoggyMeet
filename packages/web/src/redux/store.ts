7a5c920b47c5649e34dce555ac69a3cd24bc801d
Piotr Kniaź <piotrkniaz@Piotrs-MacBook-Pro.local>
2022-02-19 11:06:07 +0100

    chore: redux logger add

--- packages/web/src/redux/store.ts
+++ packages/web/src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import modalReducer from '@redux/modal/modal.slice';
+import logger from 'redux-logger';
 
 export const store = configureStore({
   reducer: {
     modal: modalReducer,
   },
+  middleware: [logger] as const,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
