efdab30ef9f605669ba37c86e11606b424ff65fd
Piotr Kniaź <piotrkniaz@Piotrs-MacBook-Pro.local>
2022-02-19 11:07:18 +0100

    chore: add chakraProdiver to creareWrapper

--- packages/web/src/utils/tests/createWrapper.tsx
+++ packages/web/src/utils/tests/createWrapper.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { render } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '../../theme';
 
 const createTestQueryClient = () =>
   new QueryClient({
@@ -14,7 +16,13 @@ const createTestQueryClient = () =>
 
 export function renderWithClient(ui: React.ReactElement) {
   const testQueryClient = createTestQueryClient();
-  const { rerender, ...result } = render(<QueryClientProvider client={testQueryClient}>{ui}</QueryClientProvider>);
+  const { rerender, ...result } = render(
+    <QueryClientProvider client={testQueryClient}>
+      <ChakraProvider resetCSS theme={theme}>
+        {ui}
+      </ChakraProvider>
+    </QueryClientProvider>,
+  );
   return {
     ...result,
     rerender: (rerenderUi: React.ReactElement) =>
