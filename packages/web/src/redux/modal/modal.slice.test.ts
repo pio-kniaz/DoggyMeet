b55d7ff67d3786dd8fa1c679343ddf338144d9fd
Piotr Kniaź <piotrkniaz@Piotrs-MacBook-Pro.local>
2022-02-19 15:26:29 +0100

    test: modal reducer test add

---
+++ packages/web/src/redux/modal/modal.slice.test.ts
@@ -0,0 +1,21 @@
+import modalReducer, { openModal, ModalTypes } from '@/redux/modal/modal.slice';
+
+describe('Modal reducer tests', () => {
+  const initialState = {
+    modalType: null,
+    modalProps: {},
+  };
+  it('Should return initial state', () => {
+    expect(
+      modalReducer(undefined, {
+        type: undefined,
+      }),
+    ).toEqual(initialState);
+  });
+  it('Should handle a SING UP modal being opened', () => {
+    expect(modalReducer(initialState, openModal({ modalType: ModalTypes.SIGN_UP }))).toEqual({
+      modalType: ModalTypes.SIGN_UP,
+      modalProps: {},
+    });
+  });
+});
