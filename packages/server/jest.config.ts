b2d0b6bc1e2e3e0e2cc0da4e7b254a59f737f4a0
Piotr Kniaź <piotrkniaz@Piotrs-MacBook-Pro.local>
2022-03-08 20:59:38 +0100

    chore: disable winston logs in test env

--- packages/server/jest.config.ts
+++ packages/server/jest.config.ts
@@ -143,7 +143,10 @@ export default {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: ['dotenv/config'],
+  setupFilesAfterEnv: [
+    'dotenv/config',
+    '<rootDir>/src/utils/tests/setupTests.ts',
+  ],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
