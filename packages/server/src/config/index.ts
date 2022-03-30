7c16a2fa34897f0e75504b20c4d6cc80f9c7be41
Piotr Kniaź <pio.kniaz@gmail.com>
2022-03-30 20:44:55 +0200

    chore: config update

--- packages/server/src/config/index.ts
+++ packages/server/src/config/index.ts
@@ -6,4 +6,7 @@ dotenv.config();
 export const config = {
   PORT: process.env.PORT,
   MONGO_URI: process.env.MONGO_URI,
+  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+  NODE_ENV: process.env.NODE_ENV,
 };
