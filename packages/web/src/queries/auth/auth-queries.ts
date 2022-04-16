b0458489a7d8265d805b987029fc5099314f53e3
Piotr Kniaź <pio.kniaz@gmail.com>
2022-04-16 20:42:20 +0200

    feat: front auth-queries update

--- packages/web/src/queries/auth/auth-queries.ts
+++ packages/web/src/queries/auth/auth-queries.ts
@@ -1,4 +1,4 @@
-import { useMutation } from 'react-query';
+import { useMutation, UseMutationOptions } from 'react-query';
 import { Api } from '@services/index';
 import { ISuccessResponse, IApiError } from '@interfaces/index';
 import { Signin } from '@components/modal/signin/signin-form/signinValidationSchema';
@@ -9,16 +9,17 @@ export interface IAuthLoginResponse extends ISuccessResponse {
   accessToken: string;
 }
 
-const authMethods = {
+export const authMethods = {
   login: async (payload: Signin): Promise<IAuthLoginResponse> => {
     const { data } = await Api.mutate<Signin, IAuthLoginResponse>(`${baseUrl}/login`, 'post', payload);
     return data;
   },
+  refreshToken: async (): Promise<IAuthLoginResponse> => {
+    const { data } = await Api.mutate<undefined, IAuthLoginResponse>(`${baseUrl}/refresh-token`, 'post');
+    return data;
+  },
 };
 
-export const useAuthLogin = () =>
-  useMutation<IAuthLoginResponse, IApiError, Signin>(authMethods.login, {
-    onSuccess: (data: IAuthLoginResponse) => {
-      console.log(data.accessToken);
-    },
-  });
+export const useAuthLogin = () => useMutation<IAuthLoginResponse, IApiError, Signin>(authMethods.login);
+export const useRefreshToken = (options?: Omit<UseMutationOptions<IAuthLoginResponse, IApiError>, 'mutationFn'>) =>
+  useMutation<IAuthLoginResponse, IApiError>(authMethods.refreshToken, options);
