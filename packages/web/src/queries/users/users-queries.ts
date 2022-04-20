aa4b97f6a2e16be08e14737f0cf2723e0ad919e6
Piotr Kniaź <pio.kniaz@gmail.com>
2022-04-20 20:44:24 +0200

    feat: mock functions add

--- packages/web/src/queries/users/users-queries.ts
+++ packages/web/src/queries/users/users-queries.ts
@@ -1,6 +1,6 @@
-import { useMutation } from 'react-query';
-import { Api } from '@services/index';
+import { useMutation, useQuery } from 'react-query';
 import { ISuccessResponse, IApiError } from '@interfaces/index';
+import { Api } from '@/utils/services/api';
 import { Signup } from '@/components/modal/signup/signup-form/signupValidationSchema';
 
 const baseUrl = '/users';
@@ -13,10 +13,18 @@ export interface IUserCreateResponse extends ISuccessResponse {
   };
 }
 
-const usersMethod = {
+const userKeys = {
+  all: [baseUrl] as const,
+};
+
+export const usersMethod = {
   create: async (payload: Signup): Promise<IUserCreateResponse> => {
     const { data } = await Api.mutate<Signup, IUserCreateResponse>(baseUrl, 'post', payload);
-    console.log(data);
+    return data;
+  },
+  // TODO: REMOVE FOR DEV PURPOSE
+  getAllUsers: async (): Promise<any> => {
+    const { data } = await Api.privateQuery<Signup, IUserCreateResponse>(baseUrl, 'get');
     return data;
   },
 };
@@ -24,3 +32,8 @@ const usersMethod = {
 // eslint-disable-next-line prettier/prettier
 export const useUserCreate = () =>
   useMutation<IUserCreateResponse, IApiError, Signup>((data) => usersMethod.create(data));
+
+// TODO: REMOVE FOR DEV PURPOSE
+export const useGetUsers = () => {
+  return useQuery<unknown, IApiError>(userKeys.all, usersMethod.getAllUsers);
+};
