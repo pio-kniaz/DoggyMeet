c81cffdfd8dd4eb65a3f1c0d20e97381556075e6
Piotr Kniaź <pio.kniaz@gmail.com>
2022-04-20 20:30:13 +0200

    feat: web check is user loged in routes component add

--- packages/web/src/routes/Routes.tsx
+++ packages/web/src/routes/Routes.tsx
@@ -1,23 +1,51 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React, { Suspense, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { useAppSelector } from '@hooks/useRedux';
+import { useRefreshToken } from '@queries/auth/auth-queries';
+import { useAppDispatch, useAppSelector } from '@hooks/useRedux';
 import { Loader } from '@components/shared';
-import { authSelector } from '@/redux/auth/auth.slice';
-import withPersistUser from '@/hoc/withPeristUserr';
+import { logout, authSelector, setUser } from '@/redux/auth/auth.slice';
 
 const AuthenticatedRoutes = React.lazy(() => import('@routes/AuthenticatedRoutes'));
 const UnAuthenticatedRoutes = React.lazy(() => import('@routes/UnAuthenticatedRoutes'));
 
 function Routes() {
-  const authState = useAppSelector(authSelector);
-  const routes = authState?.accessToken ? <AuthenticatedRoutes /> : <UnAuthenticatedRoutes />;
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const { mutateAsync } = useRefreshToken();
+  const [isLoading, setIsLoading] = useState(true);
+  const { accessToken } = useAppSelector(authSelector);
+
+  useEffect(() => {
+    const checkToken = async () => {
+      try {
+        const { accessToken: accessTokenResp } = await mutateAsync();
+        dispatch(setUser({ accessToken: accessTokenResp }));
+      } catch (error) {
+        dispatch(logout());
+        navigate('/', { replace: true });
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    if (!accessToken) {
+      checkToken();
+    } else {
+      setIsLoading(false);
+    }
+  }, [accessToken, dispatch, mutateAsync, navigate]);
+
+  if (isLoading) {
+    return <Loader fullscreen size="xl" />;
+  }
 
   return (
-    <BrowserRouter>
-      <Suspense fallback={<Loader fullscreen size="xl" />}>{routes}</Suspense>
-    </BrowserRouter>
+    <>
+      <Suspense fallback={<Loader fullscreen size="xl" />}>
+        {!accessToken ? <UnAuthenticatedRoutes /> : <AuthenticatedRoutes />}
+      </Suspense>
+    </>
   );
 }
 
-export default withPersistUser(Routes);
+export default Routes;
