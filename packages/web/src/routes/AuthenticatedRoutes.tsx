0847edc3f275cbaed940e3a76fd3c11689b0c04e
Piotr Kniaź <pio.kniaz@gmail.com>
2022-04-26 20:21:30 +0200

    refactor: add lazyloading for pages in Authenticated Routes

--- packages/web/src/routes/AuthenticatedRoutes.tsx
+++ packages/web/src/routes/AuthenticatedRoutes.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import MainLayout from '@components/layouts/main-layout/MainLayout';
-import UsersPage from '@pages/users/UsersPage';
-import NotFoundPage from '@pages/not-found/NotFoundPage';
+
+const UserPage = React.lazy(() => import('@pages/users/UsersPage'));
+const NotFoundPage = React.lazy(() => import('@pages/not-found/NotFoundPage'));
 
 function AuthenticatedRoutes() {
   return (
     <div data-testid="authenticatedRoutes">
       <Routes>
         <Route path="/" element={<MainLayout />}>
-          <Route path="" element={<UsersPage />} />
+          <Route index element={<UserPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
