d80b4dbfa61ddba95c62c929d715a58666b68683
Piotr Kniaź <pio.kniaz@gmail.com>
2022-04-26 20:25:42 +0200

    refactor: add lazyloading for pages in UnAuthenticated Routes

--- packages/web/src/routes/UnAuthenticatedRoutes.tsx
+++ packages/web/src/routes/UnAuthenticatedRoutes.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import HomePage from '@pages/home/HomePage';
-import NotFoundPage from '@pages/not-found/NotFoundPage';
 import BasicLayout from '@components/layouts/basic-layout/BasicLayout';
+import HomePage from '@pages/home/HomePage';
+
+const NotFoundPage = React.lazy(() => import('@pages/not-found/NotFoundPage'));
 
 function UnAuthenticatedRoutes() {
   return (
     <div data-testid="unAuthenticatedRoutes">
       <Routes>
         <Route path="/" element={<BasicLayout />}>
-          <Route path="" element={<HomePage />} />
+          <Route index element={<HomePage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
