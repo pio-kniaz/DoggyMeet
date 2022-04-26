8f4e40d3db935f2f1ec62d34c1d3416c6230f6e0
Piotr Kniaź <pio.kniaz@gmail.com>
2022-04-26 20:20:43 +0200

    refactor: routes component update replace Loader by Placeholder remove suspense

--- packages/web/src/routes/Routes.tsx
+++ packages/web/src/routes/Routes.tsx
@@ -1,14 +1,13 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { useRefreshToken } from '@queries/auth/auth-queries';
 import { useGetMe } from '@queries/users/users-queries';
 import { useAppDispatch, useAppSelector } from '@hooks/useRedux';
-import { Loader } from '@components/shared';
+import AuthenticatedRoutes from '@routes/AuthenticatedRoutes';
+import UnAuthenticatedRoutes from '@routes/UnAuthenticatedRoutes';
 import { clearAccessToken, authSelector, setAccessToken, setUser } from '@/redux/auth/auth.slice';
-
-const AuthenticatedRoutes = React.lazy(() => import('@routes/AuthenticatedRoutes'));
-const UnAuthenticatedRoutes = React.lazy(() => import('@routes/UnAuthenticatedRoutes'));
+import { PlaceHolder } from './Routes.styles';
 
 function Routes() {
   const navigate = useNavigate();
@@ -37,18 +36,11 @@ function Routes() {
     },
   });
 
-  // TODO: Content visibility hidden;
   if (isLoading || isGetMeLoading) {
-    return <Loader fullscreen size="xl" />;
+    return <PlaceHolder data-testid="routes-placeholder" />;
   }
 
-  return (
-    <>
-      <Suspense fallback={<Loader fullscreen size="xl" />}>
-        {!accessToken ? <UnAuthenticatedRoutes /> : <AuthenticatedRoutes />}
-      </Suspense>
-    </>
-  );
+  return <>{!accessToken ? <UnAuthenticatedRoutes /> : <AuthenticatedRoutes />}</>;
 }
 
 export default Routes;
