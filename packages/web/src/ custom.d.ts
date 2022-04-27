fba388754b7afd19ac5923ea0362b0d47ec58f95
Piotr <30327636+pio-kniaz@users.noreply.github.com>
2022-04-27 21:06:25 +0200

    Feature/current user info (#33)
    * chore: theme types add
    
    * feat: header update add user drop down

--- packages/web/src/ custom.d.ts
+++ packages/web/src/ custom.d.ts
@@ -6,3 +6,7 @@ declare module '*.jpg' {
   const content: any;
   export default content;
 }
+declare module '*.jpeg' {
+  const content: any;
+  export default content;
+}
--- packages/web/src/pages/users/UsersPage.tsx
+++ packages/web/src/pages/users/UsersPage.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
+import { Container, Box } from '@chakra-ui/react';
 import { useGetUsers } from '@queries/users/users-queries';
 
 // TODO: REMOVE FOR DEV PURPOSE
 function UsersPage() {
   const { isLoading, data, error, refetch, isRefetching } = useGetUsers();
   return (
-    <div>
-      <button type="button" onClick={() => refetch()}>
-        REFETCH
-      </button>
-      <br />
-      <pre>
-        {JSON.stringify(isRefetching, null, 2)}
+    <Box overflowY="auto">
+      <Container maxW="container.xl" flexGrow={1} width="100%">
+        <button type="button" onClick={() => refetch()}>
+          REFETCH
+        </button>
         <br />
-        {JSON.stringify(isLoading, null, 2)}
-        <br />
-        {JSON.stringify(error, null, 2)}
-        <br />
-        {JSON.stringify(data, null, 2)}
-      </pre>
-      UsersPage
-    </div>
+        <pre>
+          {JSON.stringify(isRefetching, null, 2)}
+          <br />
+          {JSON.stringify(isLoading, null, 2)}
+          <br />
+          {JSON.stringify(error, null, 2)}
+          <br />
+          {JSON.stringify(data, null, 2)}
+        </pre>
+        UsersPage
+      </Container>
+    </Box>
   );
 }
 
