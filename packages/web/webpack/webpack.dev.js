37ea6a9e48f6001827b719d7ec3b2282a3bab689
Piotr Kniaź <pio.kniaz@gmail.com>
2022-05-19 22:47:19 +0200

    chore: webpack update

--- packages/web/webpack/webpack.dev.js
+++ packages/web/webpack/webpack.dev.js
@@ -3,26 +3,28 @@ const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin'
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 const ESLintPlugin = require('eslint-webpack-plugin')
 
-module.exports = {
-  mode: 'development',
-  devtool: 'cheap-module-source-map',
-  devServer: {
-    hot: true,
-    open: true,
-    port: 8081,
-    historyApiFallback: true,
-  },
-  plugins: [
-    new ReactRefreshWebpackPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.__DEV__': true,
-    }),
-    new ForkTsCheckerWebpackPlugin({
-      async: false,
-    }),
-    new ESLintPlugin({
-      extensions: ['js', 'jsx', 'ts', 'tsx'],
-      emitWarning: false,
-    }),
-  ],
+module.exports = () => {
+  return {
+    mode: 'development',
+    devtool: 'cheap-module-source-map',
+    devServer: {
+      hot: true,
+      open: true,
+      port: 8081,
+      historyApiFallback: true,
+    },
+    plugins: [
+      new ReactRefreshWebpackPlugin(),
+      new webpack.DefinePlugin({
+        'process.env.__DEV__': true,
+      }),
+      new ForkTsCheckerWebpackPlugin({
+        async: false,
+      }),
+      new ESLintPlugin({
+        extensions: ['js', 'jsx', 'ts', 'tsx'],
+        emitWarning: false,
+      }),
+    ],
+  }
 }
